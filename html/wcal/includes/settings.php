02e9265ffdb9ed25c0e14242de6258a3b86ec7cc
Usuario <usuario@virtual.lab.inf.uva.es>
2018-02-22 16:39:27 +0100

    Añado la carpeta html

---
+++ html/wcal/includes/settings.php
@@ -0,0 +1,16 @@
+<?php
+/* updated via install/index.php on Thu, 15 Feb 2018 11:57:33 -0500
+install_password: c80c7d5e801ff686643a20b33710d3ec
+db_type: mysqli
+db_host: localhost
+db_database: webcalendar
+db_login: webcalendar
+db_password: rendimiento2018
+db_persistent: true
+db_cachedir: /tmp
+readonly: false
+user_inc: user.php
+use_http_auth: false
+single_user: false
+# end settings.php */
+?>
